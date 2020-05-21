25d170070edf0544f78f5cde7a1780ceaafe90e3
Jasmine Terry <jasmine-terry91@gmail.com>
2020-05-21 00:48:16 -0400

    zeroes passed. making change start

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
+'''
+ figure out exactly how many ways there are to make change for the amount of money they plopped down in front of you using only pennies, nickels, dimes, quarters, and half-dollars. 
+--Since this is a bank, you have an infinite supply of coinange. Write a function `making_change` that receives as input an amount of money in cents as well as an array of coin denominations and calculates the total number of ways in which change can be made for the input amount using the given coin denominations. 
 
+For example, `making_change(10)` should return 4, since there are 4 ways to make change for 10 cents using pennies, nickels, dimes, quarters, and half-dollars:
+'''
 import sys
 
 def making_change(amount, denominations):
-  # Your code here
-
-  pass
+    
 
 
 if __name__ == "__main__":
